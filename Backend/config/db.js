3bae21efbce72c76debdbb41230574ceb05f2114
Francisco Moncada B <fmoncadabuelvas0112@gmail.com>
2022-05-14 19:14:45 -0500

    .env config

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
      try {
-          const connection = await mongoose.connect('mongodb+srv://adminToe:Frankmoncada01@lyricstoe.hs2ow.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+          const connection = await mongoose.connect('process.env.MONGO_URI', 
                {
                useNewUrlParser: true,
                useUnifiedTopology: true,
