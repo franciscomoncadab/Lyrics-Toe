2564a80ec82141916fc16158fedc74b021a9f48f
Francisco Moncada B <fmoncadabuelvas0112@gmail.com>
2022-05-15 00:06:15 -0500

    hash password with bcrypt

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,7 +1,13 @@
 import User from '../models/User.js'
 
 const registerUser = async (req, res) => {
-     
+     const { email } = req.body
+     const userExists = await User.findOne({ email})
+
+     if (userExists) {
+          const error = new Error('Usuario Registrado');
+          return res.status(400).json({ msg: error.message})
+     }
      try {
           const user = new User(req.body)
           const userId = await user.save()
@@ -11,7 +17,7 @@ const registerUser = async (req, res) => {
 
      }
      
-}
+};
 
 export {
      registerUser,    
--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema({
      nombre: {
@@ -33,6 +34,14 @@ const UserSchema = new mongoose.Schema({
         timestamps: true,
       }
 );
+
+UserSchema.pre('save', async function(next) {
+     if(this.isModified('password')) {
+          next();
+     }
+     const salt = await bcrypt.genSalt(10);
+     this.password = await bcrypt.hash(this.password, salt);
+})
 
 const User = mongoose.model('User', UserSchema)
 
