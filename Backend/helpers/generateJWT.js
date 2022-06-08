e37b9f5e1570e7219f91ba60a22d24ffa334af80
Francisco Moncada B <71296717+franciscomoncadab@users.noreply.github.com>
2022-06-08 11:05:39 -0500

    Delete Backend directory

--- Backend/helpers/generateJWT.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-const generateJWT = (id) => {
-     return jwt.sign({ id }, process.env.JWT_SECRET, {
-          expiresIn: "7d"
-     })
-};
-
-export default generateJWT
